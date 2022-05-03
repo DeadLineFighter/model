96693c43798db0db49cef5a0c39e0b9a06230db1
william-yeung0916 <90163971+william-yeung0916@users.noreply.github.com>
2022-05-03 17:57:37 +0800

    without year and month: get all crime data

--- db.py
+++ db.py
@@ -1,2 +1,20 @@
-print("test")
-print("test")
+import pymongo
+
+url="mongodb://ia.dsa.21.a:tuJ6ZdJGWrEf8SAd6gb8ZaHUcs83HHJu@18.189.210.178:27017/?authSource=IA&readPreference=primary&appname=MongoDB%20Compass%20Community&directConnection=true&ssl=false"
+client = pymongo.MongoClient(url)
+db = client["UKdata"]
+dbCrime = db["Crime"]
+
+listCrime = dbCrime.distinct('Crime type')
+
+countCrime = dbCrime.count_documents({'Crime type':listCrime[0]})
+
+numCrime = []
+
+for i in range(len(listCrime)):
+    numCrime.append(dbCrime.count_documents({'Crime type':listCrime[i]}))
+
+crimeDate = zip(listCrime, numCrime)
+dictCrimeDate = dict(crimeDate)
+
+dictCrimeDate
