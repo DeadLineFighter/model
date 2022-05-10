49ec265ef622bed7ff43b97460beb79ea6ff0fc8
SeaKaByt <88830273+SeaKaByt@users.noreply.github.com>
2022-05-10 17:26:11 +0800

    update plotly

--- plotlyFunction.py
+++ plotlyFunction.py
@@ -1,10 +1,53 @@
 from re import M
 import sys
 import os
+from db import *
+import pymongo
+import plotly.express as px
+import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
+
 sys.path.append(sys.path.append(os.path.dirname(os.path.abspath(__file__))))
 
-from db import *
+url="mongodb://ia.dsa.21.a:tuJ6ZdJGWrEf8SAd6gb8ZaHUcs83HHJu@18.189.210.178:27017/?authSource=IA&readPreference=primary&appname=MongoDB%20Compass%20Community&directConnection=true&ssl=false"
+client = pymongo.MongoClient(url)
+db = client["UKdata"]
+dbCrime = db["uk_crime"]
+dbGooPOI = db["GooglePOI"]
+school = db["UK_Schools"]
+geoCol = db["UkGEO"]
 
+testDb = client["IA"]
+propertyCol = testDb["Rightmove_15cities Backup"]
+
+#---------------------
+
+def changeLatLong(postcode):
+
+    dbGeometry = geoCol.find({"name":postcode})
+    list_dbGeometry = list(dbGeometry)
+
+    for i in range(len(list_dbGeometry[0]["geometry"]["coordinates"][0])):
+        list_dbGeometry[0]["geometry"]["coordinates"][0][i][0], list_dbGeometry[0]["geometry"]["coordinates"][0][i][1] = list_dbGeometry[0]["geometry"]["coordinates"][0][i][1], list_dbGeometry[0]["geometry"]["coordinates"][0][i][0]
+
+    result = list_dbGeometry[0]["geometry"] #change place
+
+    return result
+#----------------------
+
+def countMonthCrime(postcode): #suggest use linechart to plotly
+
+    monthCrime = dbCrime.aggregate([
+        {"$match":{"$and":[{"postcode":postcode},{"date":{"$lte":"2019-06"}}]}}, #as after 2019-06, the date is not real
+        {"$project":{"all_crime&asb":1,"date":"$date","_id":0}},
+        {"$sort":{"_id":-1}}
+    ])
+
+    return list(monthCrime) #e.g countMonthCrime("BL0")
+
+
+#---
 def crime_month_line(postcode):
     df = pd.DataFrame(countMonthCrime(postcode))
     fig = px.line(df, x="date", y="all_crime&asb",
@@ -14,6 +57,12 @@ def crime_month_line(postcode):
         },
     title = "Number of crime in every month in "+postcode)
     return fig
+
+crime_month_line("BL0").write_html('first_figure.html', auto_open=True)
+
+
+
+
 
 def crime_many_month_line(postcodes):
     list_of_dfs = list()
